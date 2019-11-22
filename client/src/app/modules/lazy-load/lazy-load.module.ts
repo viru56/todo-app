d789810a7c94f22c1b6a55b6f0be242b946c4fa2
Virender Nehra <nehra.virender@gmail.com>
2019-11-22 14:46:50 +0530

    fixing issue with lazy load in production

--- client/src/app/modules/lazy-load/lazy-load.module.ts
+++ client/src/app/modules/lazy-load/lazy-load.module.ts
@@ -6,25 +6,16 @@ import { AuthGuard } from "src/app/shared";
 const routes: Routes = [
   {
     path: "login",
-    loadChildren: () =>
-      import("../login/login.module")
-        .then(m => m.LoginModule)
-        .catch(console.log)
+    loadChildren : "../login/login.module#LoginModule"
   },
   {
     path: "register",
-    loadChildren: () =>
-      import("../register/register.module")
-        .then(m => m.RegisterModule)
-        .catch(console.log)
+    loadChildren : "../register/register.module#RegisterModule"
   },
   {
     path: "task",
     canActivate: [AuthGuard],
-    loadChildren: () =>
-      import("../task/task.module")
-        .then(m => m.TaskModule)
-        .catch(console.log)
+    loadChildren : "../task/task.module#TaskModule"
   }
 ];
 
